f8757b64d66ea484755c747fab86675690a3da60
Patrick Larsen <patrick@beachdaystudios.com>
2020-10-29 02:00:01 -0600

    Learnt about syncing vars and RPCs

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        MyNetworkPlayer player = conn.identity.GetComponent<MyNetworkPlayer>();
+
+        player.SetDisplayName($"Player{numPlayers}");
+        player.SetPlayerColor(Random.ColorHSV());
+
+        Debug.Log($"There are now {numPlayers} players");
+    }
+}
---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using TMPro;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SerializeField] private TMP_Text textDisplayName = null;
+    [SerializeField] private Renderer displayColorRenderer = null;
+
+    [SyncVar(hook=nameof(HandleNameUpdated))] 
+    [SerializeField] private string displayName = "Name";
+
+    [SyncVar(hook=nameof(HandleDisplayColorUpdated))]
+    [SerializeField] private Color playerColor;
+
+    #region ServerMethods
+    [Server]
+    public void SetDisplayName(string newDisplayName)
+    {
+        displayName = newDisplayName;
+    }
+
+    [Server]
+    public void SetPlayerColor(Color newColor)
+    {
+        playerColor = newColor;
+    }
+
+    [Command]
+    private void CmdSetDisplayName(string newDisplayName)
+    {
+        RpcLogNewName(displayName);
+        SetDisplayName(newDisplayName);
+    }
+
+    #endregion
+
+    #region  ClientMethods
+
+    private void HandleDisplayColorUpdated(Color oldColor, Color newColor)
+    {
+        displayColorRenderer.material.SetColor("_BaseColor", newColor);
+    }
+
+    private void HandleNameUpdated(string oldName, string newName)
+    {
+        textDisplayName.text = newName;
+    }
+
+    [ContextMenu("Set My Name")]
+    private void SetMyName()
+    {
+        CmdSetDisplayName("My New Name");
+    }
+
+    [ClientRpc]
+    private void RpcLogNewName(string newName)
+    {
+        Debug.Log(newName);
+    }
+
+    #endregion
+
+    
+}
