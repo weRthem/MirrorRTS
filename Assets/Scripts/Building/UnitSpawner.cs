194fae8907b53a2c4564d73535177e5021e9446d
Patrick Larsen <patrick@beachdaystudios.com>
2020-11-02 01:28:00 -0600

    added unit spawning

---
+++ Assets/Scripts/Building/UnitSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject unitPrefab = null;
+    [SerializeField] private Transform spawnPoint = null;
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        GameObject unitInstance = Instantiate(unitPrefab, spawnPoint.position, spawnPoint.rotation);
+
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+    }
+
+    #endregion
+
+    #region Client
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked");
+
+        if(!hasAuthority) return;
+        if(eventData.button != PointerEventData.InputButton.Left) return;
+
+        CmdSpawnUnit();
+    }
+
+    #endregion
+}
